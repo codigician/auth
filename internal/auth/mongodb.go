6463e7936ee75875d1d3b45d74be48531e4c6c86
gokcelb <gokcelbilgin@gmail.com>
2022-01-05 11:22:50 +0300

    Refactored code by adding mongo connect and disconnect methods

--- internal/auth/mongodb.go
+++ internal/auth/mongodb.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/docker/distribution/uuid"
 	"go.mongodb.org/mongo-driver/bson"
@@ -14,80 +15,45 @@ import (
 type Mongo struct{}
 
 func (m Mongo) Save(u *User) error {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	client, ctx, collection := m.connect()
+	insertResult, err := collection.InsertOne(ctx, u)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	fmt.Println("connected to mongodb")
-	collection := client.Database("auth").Collection("users")
-
-	insertResult, err := collection.InsertOne(context.TODO(), u)
 	fmt.Println("inserted a single document:", insertResult.InsertedID)
-
-	if err := client.Disconnect(context.TODO()); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("connection to mongodb closed")
-
+	err = m.disconnect(client, ctx)
 	return err
 }
 
-func (m Mongo) Find(e string) (*User, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println("connected to mongodb")
-	collection := client.Database("auth").Collection("users")
-
+func (m Mongo) Find(email string) (*User, error) {
+	client, ctx, collection := m.connect()
 	var result *User
-	filter := bson.M{"email": e}
-	fmt.Println(e)
-	err = collection.FindOne(context.TODO(), filter).Decode(&result)
+	filter := bson.M{"email": email}
+	fmt.Println(email)
+	err := collection.FindOne(ctx, filter).Decode(&result)
 	if err != nil {
 		return nil, err
 	}
 	fmt.Printf("Found a single document: %+v/n", result)
-	fmt.Printf(result.Firstname, result.Lastname, result.Email, result.HashedPassword)
-
-	if err := client.Disconnect(context.TODO()); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("connection to mongodb closed")
-
-	return result, nil
+	err = m.disconnect(client, ctx)
+	return result, err
 }
 
 func (m Mongo) Update(id uuid.UUID, key string, value string) error {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("connected to mongodb")
-	collection := client.Database("auth").Collection("users")
-
+	client, ctx, collection := m.connect()
 	filter := bson.M{"id": bson.M{"$eq": id}}
 	updater := bson.M{"$set": bson.M{key: value}}
-	result, err := collection.UpdateOne(context.TODO(), filter, updater)
+	result, err := collection.UpdateOne(ctx, filter, updater)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	fmt.Println("RESULT:", result)
-	return nil
+	err = m.disconnect(client, ctx)
+	return err
 }
 
 func (m Mongo) List() {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("connected to mongodb")
-	collection := client.Database("auth").Collection("users")
-
+	client, ctx, collection := m.connect()
 	var results []*User
 	cursor, err := collection.Find(context.TODO(), bson.M{})
 	if err != nil {
@@ -101,8 +67,29 @@ func (m Mongo) List() {
 		results = append(results, &user)
 	}
 	cursor.Close(context.TODO())
-
 	for _, r := range results {
 		fmt.Println("One user:", r.ID, r.Firstname, r.Lastname, r.Email, r.HashedPassword)
 	}
+	m.disconnect(client, ctx)
+}
+
+func (m Mongo) connect() (mongo.Client, context.Context, mongo.Collection) {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.NewClient(clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	usersCollection := client.Database("auth").Collection("users")
+	return *client, ctx, *usersCollection
+}
+
+func (m Mongo) disconnect(client mongo.Client, ctx context.Context) error {
+	err := client.Disconnect(ctx)
+	return err
 }
